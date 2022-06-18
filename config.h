3b278fa51eb83184c1d5797e4a537ba99a8a5187
Saumit Dinesan <justsaumit@protonmail.com>
2022-06-18 21:45:22 +0530

    Updated config.h

--- config.h
+++ config.h
@@ -2,11 +2,13 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",  "cpu",      5,  2},
   {"",  "nettraf",  1,  16},
   {"",  "memory",   10, 14}, 
   {"",  "volume",   1,  10},
   {"",  "clock",    5,  1},
   {"",  "battery",  5,  3},
+//  {"",  "iplocate",  7,  17},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
